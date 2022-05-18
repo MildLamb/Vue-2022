96deb0acf259b6d2487cfa4195b7803d98f9774f
MildLamb <1902524390@qq.com>
2022-05-18 17:11:47 +0800

    重新提交脚手架测试文件

---
+++ Vue_basic/vue脚手架/vue_test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Master></Master>
+        <MyRole></MyRole>
+    </div>
+</template>
+
+<script>
+    // 引入组件
+    import Master from "./components/Master";
+    import MyRole from "./components/Role";
+
+    export default {
+        name: "MyApp",
+        components: {
+            Master,
+            MyRole
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
